f87af0231a4507c5921fed6da12af7748f078adf
Marifel Angeles <mmarifel@gmail.com>
2018-12-13 12:51:18 -0600

    how to exclude folder

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Hello World!");
+
+var varVariable = “this is a variable”;
+let letVariable = “this is a another variable”;
+
+console.log(window.varVariable); //this is a var variable
+console.log(window.letVariable); //undefined
